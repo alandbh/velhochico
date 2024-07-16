7bd9888395a59b09d57c19706e49aec10f392e79
Pedro Vasconcelos <pedr720@gmail.com>
2024-07-16 15:39:08 -0300

    Criando componente Logo link

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "green-logo" : "#6FCF97"
+      },
+      borderRadius: {
+        "4xl" : "2rem"
+      }
     },
   },
   plugins: [],
