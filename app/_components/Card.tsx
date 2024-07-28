46850ed7a8b26e51aac3cb36c4e6539f5d6f70b9
Alan Vasconcelos <alandbh@gmail.com>
2024-07-27 22:20:43 -0300

    Making card responsive

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -2,24 +2,30 @@ import Image from "next/image";
 import Button from "./Button";
 
 type CardProps = {
-  image: string;
-  title: string;
-  children: React.ReactNode;
+    image: string;
+    title: string;
+    children: React.ReactNode;
 };
 
 const Card = ({ image, title, children }: CardProps) => {
-  return (
-    <div className="w-full h-[21rem] grid grid-cols-12 gap-x-5">
-      <div className="flex flex-col self-center gap-y-[0.9rem] content col-span-5 col-start-2 text-darkest-blue pl-8">
-        <h2 className="text-4xl">{title}</h2>
-        <p>{children}</p>
-        <Button text="confira" style="primary" href="#"></Button>
-      </div>
-      <div className="relative col-span-5 w-auto">
-        <Image className="object-cover" alt="" src={image} quality={100} fill />
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full md:h-[21rem] grid grid-cols-12 md:gap-x-5 gap-x-3 px-3 md:px-0">
+            <div className="flex flex-col self-center gap-y-[0.9rem] content md:col-span-5 col-span-8 md:col-start-2 text-darkest-blue md:pl-8">
+                <h2 className="text-4xl">{title}</h2>
+                <p>{children}</p>
+                <Button text="confira" style="primary" href="#"></Button>
+            </div>
+            <div className="relative md:col-span-5 col-span-4 w-auto h-44 md:h-auto">
+                <Image
+                    className="object-cover"
+                    alt=""
+                    src={image}
+                    quality={100}
+                    fill
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Card;
--- app/atrativos/page.tsx
+++ app/atrativos/page.tsx
@@ -1,7 +1,13 @@
+import Card from "../_components/Card";
+
 export default function Atrativos() {
     return (
         <div>
             <h1>Página Atrativos</h1>
+            <Card image="/rede.jpg" title="Chalé de 1 quarto">
+                Chalé com varanda, vista para a serra, ar-condicionado, frigobar
+                e cozinha.
+            </Card>
         </div>
     );
 }
