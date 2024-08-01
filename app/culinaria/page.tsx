c114b235eba97e61ab32b27226ab22bf0aa93368
Pedro Vasconcelos <pedr720@gmail.com>
2024-08-01 20:57:24 -0300

    Adding tag main around the page content.

--- app/culinaria/page.tsx
+++ app/culinaria/page.tsx
@@ -21,72 +21,74 @@ export default function Culinaria() {
   return (
     <>
       <Header backgroundImg="/queijos.jpg">Culinária</Header>
-      <div className="py-22 text-center">
-        <Title>Sabor e Simplicidade No ponto</Title>
-        <div className="text-2xl mt-12 gap-y-6 flex flex-col">
-          <p>
-            Experimente os mais variados sabores da culinária mineira! Na
-            Pousada Velho Chico, você poderá se deliciar com os sabores típicos
-            da nossa terra.
-          </p>
-          <p>
-            No café da manhã, iguarias como broas, bolos, pães, doces, e o
-            queijo canastra produzido na própria pousada.
-          </p>
-          <p>
-            Além disso, você poderá aproveitar frutas colhidas diretamente do pé
-            como laranja, abacate, manga, caju, jambo, goiaba, pitanga, jaca,
-            ameixa e amora.
-          </p>
-        </div>
-      </div>
-      <div className="w-full flex justify-center">
-        <Button
-          text="reserve já"
-          icon={<IconWhatsapp color="white" />}
-          style="primary"
-          href="https://wa.me/553799290110"
-        />
-      </div>
-      <div className="py-22 text-center">
-        <Title>Para comer com os olhos</Title>
-
-        <div className="w-full grid grid-cols-12 gap-5 mt-12">
-          <div className="col-start-2 col-span-10 w-full grid grid-cols-3 grid-rows-2 grid-flow-col">
-            {images.map((image, index) => (
-              <div
-                key={index}
-                className="h-81 w-auto relative hover:cursor-pointer"
-              >
-                <Image
-                  key={index}
-                  className="object-cover"
-                  src={image.src}
-                  alt={`Image ${index + 1}`}
-                  onClick={() => setIndex(index)}
-                  fill
-                />
-              </div>
-            ))}
+      <main>
+        <div className="py-22 text-center">
+          <Title>Sabor e Simplicidade No ponto</Title>
+          <div className="text-2xl mt-12 gap-y-6 flex flex-col">
+            <p>
+              Experimente os mais variados sabores da culinária mineira! Na
+              Pousada Velho Chico, você poderá se deliciar com os sabores
+              típicos da nossa terra.
+            </p>
+            <p>
+              No café da manhã, iguarias como broas, bolos, pães, doces, e o
+              queijo canastra produzido na própria pousada.
+            </p>
+            <p>
+              Além disso, você poderá aproveitar frutas colhidas diretamente do
+              pé como laranja, abacate, manga, caju, jambo, goiaba, pitanga,
+              jaca, ameixa e amora.
+            </p>
           </div>
         </div>
+        <div className="w-full flex justify-center">
+          <Button
+            text="reserve já"
+            icon={<IconWhatsapp color="white" />}
+            style="primary"
+            href="https://wa.me/553799290110"
+          />
+        </div>
+        <div className="py-22 text-center">
+          <Title>Para comer com os olhos</Title>
 
-        <Lightbox
-          index={index}
-          open={index >= 0}
-          close={() => setIndex(-1)}
-          slides={images}
-          render={{ slide: NextJsImage }}
-        />
-      </div>
-      <div className="w-full flex justify-center mb-24">
-        <Button
-          text="reserve já"
-          icon={<IconWhatsapp color="white" />}
-          style="primary"
-          href="https://wa.me/553799290110"
-        />
-      </div>
+          <div className="w-full grid grid-cols-12 gap-5 mt-12">
+            <div className="col-start-2 col-span-10 w-full grid grid-cols-3 grid-rows-2 grid-flow-col">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className="h-81 w-auto relative hover:cursor-pointer"
+                >
+                  <Image
+                    key={index}
+                    className="object-cover"
+                    src={image.src}
+                    alt={`Image ${index + 1}`}
+                    onClick={() => setIndex(index)}
+                    fill
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <Lightbox
+            index={index}
+            open={index >= 0}
+            close={() => setIndex(-1)}
+            slides={images}
+            render={{ slide: NextJsImage }}
+          />
+        </div>
+        <div className="w-full flex justify-center mb-24">
+          <Button
+            text="reserve já"
+            icon={<IconWhatsapp color="white" />}
+            style="primary"
+            href="https://wa.me/553799290110"
+          />
+        </div>
+      </main>
       <Footer></Footer>
     </>
   );
