23c039c6b9afc3dd23889f1d0cf6f745f2b67965
Alan Vasconcelos <alandbh@gmail.com>
2024-08-18 20:41:17 -0300

    Creating Intro component

---
+++ app/_components/Intro.tsx
@@ -0,0 +1,19 @@
+import Title from "./Title";
+
+type IntroProps = {
+    title: string;
+    children: React.ReactNode;
+};
+
+const Intro = ({ title, children }: IntroProps) => {
+    return (
+        <section className="col-span-10 grid grid-cols-10 flex-col gap-y-12 items-center py-22">
+            <Title className="col-span-10">{title}</Title>
+            <div className="text-2xl col-span-8 col-start-2 text-center flex flex-col gap-12">
+                {children}
+            </div>
+        </section>
+    );
+};
+
+export default Intro;
