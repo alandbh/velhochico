0211cd4d19b4a94e804812ac8ae938b2b817a503
Alan Vasconcelos <alandbh@gmail.com>
2024-08-18 20:26:19 -0300

    Refactoring Title component

--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
 
-const Title = ({ children }: { children: ReactNode }) => {
+type TitleProps = {
+    children: ReactNode;
+    className: string;
+};
+
+const Title = ({ children, className }: TitleProps) => {
     return (
-        <div className="gap-2 flex flex-col items-center md:gap-7">
-            <div className="w-[111px] h-2 bg-dark-blue md:h-3"></div>
+        <div
+            className={`gap-2 flex flex-col items-center md:gap-7 ${className}`}
+        >
+            <div className="h-2 bg-dark-blue md:h-3 w-28"></div>
             <div className="text-dark-blue font-gideonRoman tracking-[0.5rem] text-2xl uppercase text-[1.5rem] md:text-[2.5rem] ">
                 {children}
             </div>
