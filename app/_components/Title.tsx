9c7832b9c7da01a5ad3857ea1969dea1e23f2a3d
Alan Vasconcelos <alandbh@gmail.com>
2024-08-18 20:04:39 -0300

    Fixing title's style

--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from "react";
 
 const Title = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="gap-2 flex flex-col items-center md:gap-7">
-      <div className="w-[111px] h-2  bg-dark-blue md:h-3"></div>
-      <div className="whitespace-nowrap font-inter text-dark-blue text-2xl uppercase md:text-[2.5rem] ">
-        {children}
-      </div>
-    </div>
-  );
+    return (
+        <div className="gap-2 flex flex-col items-center md:gap-7">
+            <div className="w-[111px] h-2 bg-dark-blue md:h-3"></div>
+            <div className="text-dark-blue font-gideonRoman tracking-[0.5rem] text-2xl uppercase text-[1.5rem] md:text-[2.5rem] ">
+                {children}
+            </div>
+        </div>
+    );
 };
 
 export default Title;
