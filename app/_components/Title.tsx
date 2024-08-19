d8f17bf5a08a9a40ffb80e4077fa6a58ef2dcf13
Alan Vasconcelos <alandbh@gmail.com>
2024-08-18 21:21:56 -0300

    Fixing title's line-height

--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -11,7 +11,7 @@ const Title = ({ children, className }: TitleProps) => {
             className={`gap-2 flex flex-col items-center md:gap-7 ${className}`}
         >
             <div className="h-2 bg-dark-blue md:h-3 w-28"></div>
-            <div className="text-dark-blue text-center font-gideonRoman tracking-[0.5rem] text-2xl uppercase text-[1.5rem] md:text-[2.5rem] ">
+            <div className="text-dark-blue leading-snug text-center font-gideonRoman tracking-[0.5rem] text-2xl uppercase text-[1.5rem] md:text-[2.5rem] ">
                 {children}
             </div>
         </div>
