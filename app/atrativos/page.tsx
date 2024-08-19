aa1f0166b4777c74e70d1736ca1c9e7d9498daab
Alan Vasconcelos <alandbh@gmail.com>
2024-08-19 00:41:05 -0300

    Layouting page Atrativos

--- app/atrativos/page.tsx
+++ app/atrativos/page.tsx
@@ -1,128 +1,92 @@
-"use client";
 import Image from "next/image";
 import Button from "../_components/Button";
-import Card from "../_components/Card";
 import Footer from "../_components/Footer";
 import Header from "../_components/Header";
 import IconWhatsapp from "../_components/icons/Whatsapp";
-import Title from "../_components/Title";
 import React from "react";
-import Lightbox from "yet-another-react-lightbox";
-import NextJsImage from "../_components/LightBox";
+import Intro from "../_components/Intro";
+import Article from "../_components/Article";
+import Gallery from "../_components/Gallery";
 const images = [
-  { src: "/arvore.png" },
-  { src: "/cachoeira1.png" },
-  { src: "/cachoeira2.png" },
-  { src: "/ceu.png" },
-  { src: "/queijos.jpg" },
-  { src: "/chale.jpg" },
+    { src: "/arvore.png" },
+    { src: "/cachoeira1.png" },
+    { src: "/cachoeira2.png" },
+    { src: "/ceu.png" },
+    { src: "/queijos.jpg" },
+    { src: "/chale.jpg" },
 ];
 
 export default function Atrativos() {
-  const [index, setIndex] = React.useState(-1);
-  return (
-    <>
-      <Header backgroundImg="/fusca.jpg">Atrativos</Header>
-      <main className="py-22 ">
-        <div className="text-center  w-full grid grid-cols-12 gap-5 gap-y-12">
-          <div className="col-span-12">
-            <Title>natureza, aventura e diversão</Title>
-          </div>
-          <p className="text-2xl row-start-2 col-span-8 col-start-3 text-center">
-            Venha se encantar com o majestoso Rio São Francisco, ideal para um
-            banho refrescante ou mergulho revigorante. Em apenas 18 km, explore
-            a deslumbrante Cachoeira Casca D’Anta, a maior do Parque Nacional da
-            Serra da Canastra.
-          </p>
-          <div className="w-full flex justify-center row-start-3 col-span-12">
-            <Button
-              text="reserve já"
-              icon={<IconWhatsapp color="white" />}
-              style="primary"
-              href="https://wa.me/553799290110"
-            />
-          </div>
-        </div>
-        <div className="relative h-[37.25rem] w-full mt-12">
-          <Image
-            className="object-cover object-top"
-            alt=""
-            src="/cachoeira1.png"
-            fill
-          />
-        </div>
-        <div className="w-full grid grid-cols-12 gap-5 mb-[7.625rem] bg-gray-6 pt-12 h-[536px]">
-          <div className="col-start-2 col-span-4 gap-y-10 flex flex-col">
-            <h1 className="text-4xl">Aventuras inesqueciveis</h1>
-            <p className="text-xl text">
-              Sinta a adrenalina com atividades radicais como bóia-cross e
-              aproveite o campo de futebol para momentos de descontração e
-              diversão com amigos. Se aventure nas trilhas maravilhosas e
-              passeios a cavalo, vivenciando a natureza de perto.
-            </p>
-          </div>
-          <div className="col-span-6 relative">
-            <div className="relative w-[12.75rem] h-[20.68rem]">
-              <Image alt="" src="/ponte.jpg" fill />
-            </div>
-            <div className="relative h-[225px] w-[300px] top-[-316px] left-[203px]">
-              <Image alt="" src="/futebol.jpg" fill />
-            </div>
-            <div className="relative w-[265px] h-[174px] top-[-310px] z-10">
-              <Image alt="" src="/fusca.jpg" fill />
-            </div>
-            <div className="relative w-[340px] h-[234px] top-[-520px] left-[203px]">
-              <Image alt="" src="/cavalo.jpg" fill />
-            </div>
-          </div>
-        </div>
-        <div className="w-full flex justify-center">
-          <Button
-            text="reserve já"
-            icon={<IconWhatsapp color="white" />}
-            style="primary"
-            href="https://wa.me/553799290110"
-          />
-        </div>
-        <div className="py-22">
-          <Title>imagens de tiar o folego</Title>
-          <div className="w-full grid grid-cols-12 gap-5 mt-12">
-            <div className="col-start-2 col-span-10 w-full grid grid-cols-3 grid-rows-2 grid-flow-col">
-              {images.map((image, index) => (
-                <div
-                  key={index}
-                  className="h-81 w-auto relative hover:cursor-pointer"
-                >
-                  <Image
-                    key={index}
-                    className="object-cover"
-                    src={image.src}
-                    alt={`Image ${index + 1}`}
-                    onClick={() => setIndex(index)}
-                    fill
-                  />
+    return (
+        <>
+            <Header backgroundImg="/fusca.jpg">Atrativos</Header>
+            <main className="grid grid-cols-12 px-3 md:px-5 text-darkest-blue">
+                <div className="col-span-10 grid grid-cols-10 col-start-2 ">
+                    <Intro title="natureza, aventura e diversão">
+                        <p className="text-sm md:text-[24px] leading-normal">
+                            Venha se encantar com o majestoso Rio São Francisco,
+                            ideal para um banho refrescante ou mergulho
+                            revigorante. Em apenas 18 km, explore a deslumbrante
+                            Cachoeira Casca D’Anta, a maior do Parque Nacional
+                            da Serra da Canastra.
+                        </p>
+                    </Intro>
+                    <div className="col-span-10 flex flex-col gap-12 pb-22">
+                        <div className="flex items-center justify-center">
+                            <Button
+                                text="reserve já"
+                                icon={<IconWhatsapp color="white" />}
+                                style="primary"
+                                href="https://wa.me/553799290110"
+                            />
+                        </div>
+                    </div>
                 </div>
-              ))}
+            </main>
+            <div className="relative h-[37.25rem] w-full mt-12">
+                <Image
+                    className="object-cover object-top"
+                    alt=""
+                    src="/cachoeira1.png"
+                    fill
+                />
             </div>
-          </div>
-        </div>
-        <Lightbox
-          index={index}
-          open={index >= 0}
-          close={() => setIndex(-1)}
-          slides={images}
-          render={{ slide: NextJsImage }}
-        />
-        <div className="w-full flex justify-center">
-          <Button
-            text="reserve já"
-            icon={<IconWhatsapp color="white" />}
-            style="primary"
-            href="https://wa.me/553799290110"
-          />
-        </div>
-      </main>
-      <Footer></Footer>
-    </>
-  );
+            <div className="w-full grid grid-cols-12 gap-5 mb-[7.625rem] bg-gray-6 pt-12">
+                <div className="col-start-2 col-span-4 gap-y-10 flex flex-col">
+                    <h1 className="text-4xl">Aventuras inesqueciveis</h1>
+                    <p className="text-xl leading-relaxed">
+                        Sinta a adrenalina com atividades radicais como
+                        bóia-cross e aproveite o campo de futebol para momentos
+                        de descontração e diversão com amigos. Se aventure nas
+                        trilhas maravilhosas e passeios a cavalo, vivenciando a
+                        natureza de perto.
+                    </p>
+                </div>
+                <div className="col-span-6 relative">
+                    <div className="md:h-[527px] md:w-[661px] relative">
+                        <Image alt="" src="/img-atrativos.png" fill />
+                    </div>
+                </div>
+            </div>
+            <main className="grid grid-cols-12 px-3 md:px-5 text-darkest-blue">
+                <div className="col-span-10 grid grid-cols-10 col-start-2 ">
+                    <Article>
+                        <div className="flex items-center justify-center">
+                            <Button
+                                text="reserve já"
+                                icon={<IconWhatsapp color="white" />}
+                                style="primary"
+                                href="https://wa.me/553799290110"
+                            />
+                        </div>
+                        <Intro title="Imagens de tirar o fôlego"></Intro>
+
+                        <Gallery slides={images} />
+                    </Article>
+                </div>
+            </main>
+
+            <Footer></Footer>
+        </>
+    );
 }
