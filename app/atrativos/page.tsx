9299816ec3200afd986c412d5fe06f44b29b0f44
Alan Vasconcelos <alandbh@gmail.com>
2024-08-20 13:30:45 -0300

    Fixing max-width os the page

--- app/atrativos/page.tsx
+++ app/atrativos/page.tsx
@@ -51,27 +51,35 @@ export default function Atrativos() {
                     fill
                 />
             </div>
-            <div className="w-full grid grid-cols-12 gap-5 mb-11 bg-gray-6 pt-12">
-                <div
-                    style={{ gridColumnStart: 2 }}
-                    className="lg:col-span-4 col-span-10 gap-y-10 flex flex-col"
-                >
-                    <h1 className="text-4xl">Aventuras inesqueciveis</h1>
-                    <p className="text-xl leading-relaxed">
-                        Sinta a adrenalina com atividades radicais como
-                        bóia-cross e aproveite o campo de futebol para momentos
-                        de descontração e diversão com amigos. Se aventure nas
-                        trilhas maravilhosas e passeios a cavalo, vivenciando a
-                        natureza de perto.
-                    </p>
-                </div>
-                <div className="col-span-9 lg:col-span-4 col-start-3 relative">
-                    <div className="w-[430px] h-[368px] md:h-[527px] md:w-[661px] relative">
-                        <Image alt="" src="/img-atrativos.png" fill />
+            <div className=" mb-11 bg-gray-6 pt-12 ">
+                <div className="w-full grid grid-cols-12 gap-5 max-w-screen-xl mx-auto">
+                    <div
+                        style={{ gridColumnStart: 2 }}
+                        className="lg:col-span-4 col-span-10 gap-y-10 flex flex-col"
+                    >
+                        <h1 className="text-4xl">Aventuras inesqueciveis</h1>
+                        <p className="text-xl leading-relaxed">
+                            Sinta a adrenalina com atividades radicais como
+                            bóia-cross e aproveite o campo de futebol para
+                            momentos de descontração e diversão com amigos. Se
+                            aventure nas trilhas maravilhosas e passeios a
+                            cavalo, vivenciando a natureza de perto.
+                        </p>
+                    </div>
+                    <div className="col-span-9 lg:col-span-7 col-start-1 relative">
+                        <div className="w-full max-w-[600px] relative">
+                            <Image
+                                className="object-fill"
+                                alt=""
+                                src="/img-atrativos.png"
+                                width={1000}
+                                height={1000}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-            <main className="grid grid-cols-12 px-3 md:px-5 text-darkest-blue">
+            <main className="grid grid-cols-12 px-3 md:px-5 text-darkest-blue max-w-screen-xl mx-auto">
                 <div className="col-span-10 grid grid-cols-10 col-start-2 ">
                     <Article>
                         <div className="flex items-center justify-center">
