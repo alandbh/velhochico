2498aa97762fbc73b61e54c06389e3574e089853
Alan Vasconcelos <alandbh@gmail.com>
2024-08-20 14:20:44 -0300

    hover effects for navLink and buttons

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,27 +1,27 @@
 import Link from "next/link";
 
 const Button = ({ text, style, href, icon }: any) => {
-  return (
-    <>
-      {style === "primary" ? (
-        <Link
-          href={href}
-          className="text-white font-montserrat bg-green-2 py-3 px-6 rounded-md font-bold uppercase flex gap-2 w-fit"
-        >
-          {icon}
-          <span className="whitespace-nowrap">{text}</span>
-        </Link>
-      ) : (
-        <Link
-          href={href}
-          className="text-green-2 py-3 px-6 rounded-md font-bold gap-2 uppercase flex border-solid border-2 border-green-2 w-fit"
-        >
-          {icon}
-          <span className="whitespace-nowrap">{text}</span>
-        </Link>
-      )}
-    </>
-  );
+    return (
+        <>
+            {style === "primary" ? (
+                <Link
+                    href={href}
+                    className="text-white font-montserrat bg-green-2 focus:outline-none focus:bg-green-700 focus:ring-4 hover:bg-green-700 hover:ring-4 ring-green-300 py-3 px-6 rounded-md font-bold uppercase flex gap-2 w-fit"
+                >
+                    {icon}
+                    <span className="whitespace-nowrap">{text}</span>
+                </Link>
+            ) : (
+                <Link
+                    href={href}
+                    className="text-green-2 hover:bg-green-100 hover:text-green-700 focus:outline-none focus:bg-green-100 focus:text-green-700 py-3 px-6 rounded-md font-bold gap-2 uppercase flex border-solid border-2 border-green-2 w-fit"
+                >
+                    {icon}
+                    <span className="whitespace-nowrap">{text}</span>
+                </Link>
+            )}
+        </>
+    );
 };
 
 export default Button;
--- app/_components/NavLink.tsx
+++ app/_components/NavLink.tsx
@@ -4,24 +4,26 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 type NavLinkProps = {
-  href: string;
-  children: React.ReactNode;
+    href: string;
+    children: React.ReactNode;
 };
 
 const NavLink = ({ children, href }: NavLinkProps) => {
-  const pathName = usePathname();
-  return (
-    <li>
-      <Link
-        className={`pb-1 px-1 text-white ${
-          pathName === href ? "border-b-2 border-white font-bold" : ""
-        }`}
-        href={href}
-      >
-        {children}
-      </Link>
-    </li>
-  );
+    const pathName = usePathname();
+    return (
+        <li>
+            <Link
+                className={`pb-1 px-1 text-white ${
+                    pathName === href
+                        ? "border-b-4 border-white font-bold hover:border-b-4"
+                        : ""
+                } hover:border-b-2 border-white`}
+                href={href}
+            >
+                {children}
+            </Link>
+        </li>
+    );
 };
 
 export default NavLink;
