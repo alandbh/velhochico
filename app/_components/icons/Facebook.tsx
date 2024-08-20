4e8cfaaf88836f5c7a38b550dc79c6f16972ab6a
Alan Vasconcelos <alandbh@gmail.com>
2024-08-20 00:29:57 -0300

    Fixing icons dimensions

--- app/_components/icons/Facebook.tsx
+++ app/_components/icons/Facebook.tsx
@@ -1,22 +1,20 @@
-import React from "react";
-
-function Facebook() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="21"
-      fill="none"
-      viewBox="0 0 12 21"
-    >
-      <path
-        fill="#fff"
-        fillRule="evenodd"
-        d="M7.553 20.66v-9.199h3.138l.45-3.591H7.552V5.62c0-1.01.336-1.795 1.795-1.795h1.908V.567c-.45 0-1.567-.109-2.805-.109a4.329 4.329 0 00-4.606 4.715v2.692H.706v3.592h3.139v9.2l3.708.004z"
-        clipRule="evenodd"
-      ></path>
-    </svg>
-  );
+function Facebook({ color = "#fff" }: { color?: string }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                fill={color}
+                fillRule="evenodd"
+                d="M13.846 22.212v-9.199h3.139l.449-3.592h-3.588V7.172c0-1.01.337-1.795 1.796-1.795h1.908V2.118c-.45 0-1.567-.108-2.806-.108a4.33 4.33 0 00-4.605 4.714v2.693H7v3.592h3.139v9.199l3.707.004z"
+                clipRule="evenodd"
+            ></path>
+        </svg>
+    );
 }
 
 export default Facebook;
