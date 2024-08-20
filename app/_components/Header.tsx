17e277c51a57e36b7234a9782c066ad5a1851341
Alan Vasconcelos <alandbh@gmail.com>
2024-08-19 23:53:55 -0300

    Header responsive

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,35 +2,40 @@ import React from "react";
 import LogoLink from "./LogoLink";
 import NavMenu from "./NavMenu";
 import Image from "next/image";
+import Hamburguer from "./icons/Hamburguer";
+import LogoLinkExtended from "./LogoLinkExtended";
 
 type HeaderProps = {
-  backgroundImg: string;
-  children: React.ReactNode;
+    backgroundImg: string;
+    children: React.ReactNode;
 };
 
 const Header = ({ children, backgroundImg }: HeaderProps) => {
-  return (
-    <div className="before:content[''] before:h-full before:w-full before:absolute before:bg-header-bg before:z-10 relative h-97 w-full   flex flex-col">
-      <Image
-        alt=""
-        src={backgroundImg}
-        quality={100}
-        fill
-        className="object-cover object-center"
-      />
-      <div className="flex justify-around h-fit items-center z-10">
-        <div className="col-start-2 col-span-2">
-          <LogoLink href="/"></LogoLink>
+    return (
+        <div className="before:content[''] before:h-full before:w-full before:absolute before:bg-header-bg before:z-10 relative h-97 w-full flex flex-col">
+            <Image
+                alt=""
+                src={backgroundImg}
+                quality={100}
+                fill
+                className="object-cover object-center"
+            />
+            <div className="hidden md:flex justify-around h-fit items-center z-10">
+                <LogoLink href="/" />
+
+                <NavMenu />
+            </div>
+            <div className="md:hidden flex justify-center z-10">
+                <LogoLinkExtended href="/" />
+                <button className="absolute right-5 top-5">
+                    <Hamburguer />
+                </button>
+            </div>
+            <h1 className="text-white z-10 font-licorice text-9xl text-center flex items-center self-center h-97">
+                {children}
+            </h1>
         </div>
-        <div className="col-start-7 flex align-baseline">
-          <NavMenu></NavMenu>
-        </div>
-      </div>
-      <h1 className="text-white z-10 font-licorice text-9xl text-center flex items-center self-center h-97">
-        {children}
-      </h1>
-    </div>
-  );
+    );
 };
 
 export default Header;
--- app/_components/NavMenu.tsx
+++ app/_components/NavMenu.tsx
@@ -1,33 +1,20 @@
 import NavLink from "./NavLink";
+import Hamburguer from "./icons/Hamburguer";
 
 const NavMenu = () => {
-  return (
-    <div>
-      <nav>
-        <ul className="flex gap-8 text-base">
-          <NavLink href="/">
-            <span>Início</span>
-          </NavLink>
-
-          <NavLink href="/acomodacoes">
-            <span>Acomodações</span>
-          </NavLink>
-
-          <NavLink href="/culinaria">
-            <span>Culinária</span>
-          </NavLink>
-
-          <NavLink href="/atrativos">
-            <span>Atrativos</span>
-          </NavLink>
-
-          <NavLink href="/contato">
-            <span>Contato</span>
-          </NavLink>
-        </ul>
-      </nav>
-    </div>
-  );
+    return (
+        <div>
+            <nav className="hidden md:block">
+                <ul className="flex gap-8 text-base">
+                    <NavLink href="/">Início</NavLink>
+                    <NavLink href="/acomodacoes">Acomodações</NavLink>
+                    <NavLink href="/culinaria">Culinária</NavLink>
+                    <NavLink href="/atrativos">Atrativos</NavLink>
+                    <NavLink href="/contato">Contato</NavLink>
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default NavMenu;
