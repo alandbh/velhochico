c4c3eb0d280d8658bba286b2f66497deadad4229
Alan Vasconcelos <alandbh@gmail.com>
2024-08-24 13:53:40 -0300

    Refactoring Card component to receive dynamic props and fixing responsiveness

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -5,17 +5,27 @@ type CardProps = {
     image: string;
     title: string;
     children: React.ReactNode;
+    url: string;
+    key?: number;
 };
 
-const Card = ({ image, title, children }: CardProps) => {
+const Card = ({ key, image, title, children, url }: CardProps) => {
     return (
-        <div className="w-full md:h-[21rem] grid grid-cols-12 md:gap-x-5 gap-x-3 px-3 md:px-0">
-            <div className="flex flex-col self-center gap-y-[0.9rem] content md:col-span-5 col-span-8 md:col-start-2 text-darkest-blue md:pl-8">
-                <h2 className="text-4xl">{title}</h2>
-                <p>{children}</p>
-                <Button text="confira" style="primary" href="#"></Button>
+        <div
+            key={key}
+            className="w-full md:h-[21rem] grid grid-cols-12 md:gap-x-5 gap-x-3 px-3 md:px-0"
+        >
+            <div className="flex flex-col self-center gap-y-[0.9rem] content md:col-span-5 col-span-7 md:col-start-2 text-darkest-blue md:pl-8">
+                <h2 className="text-xl md:text-4xl">{title}</h2>
+                <p className="max-md:text-sm">{children}</p>
+                <div className="max-md:hidden">
+                    <Button size="medium" text="confira" href={url}></Button>
+                </div>
+                <div className="md:hidden">
+                    <Button size="small" text="confira" href={url}></Button>
+                </div>
             </div>
-            <div className="relative md:col-span-5 col-span-4 w-auto h-44 md:h-auto">
+            <div className="relative md:col-span-5 col-span-5 w-auto h-auto md:h-auto">
                 <Image
                     className="object-cover"
                     alt=""
