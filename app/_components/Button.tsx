bd0919aa484355b0acae20f1cc1590a9be2c2c06
Alan Vasconcelos <alandbh@gmail.com>
2024-08-25 22:07:53 -0300

    Improving button styles

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -16,11 +16,11 @@ const Button = ({
     icon,
 }: ButtonPropsTypes) => {
     const buttonClasses = {
-        base: " focus:outline-none focus:ring-4 hover:ring-4 ring-green-300 font-montserrat font-bold uppercase flex w-fit ",
+        base: " focus:outline-none focus:ring-4 hover:ring-4 ring-green-300/50 font-montserrat font-bold uppercase flex w-fit ",
         primary:
             " bg-green-2 focus:bg-green-700 hover:bg-green-700 text-white rounded-md  ",
         regular:
-            "rounded-md text-green-2 hover:bg-green-100 hover:text-green-700 focus:bg-green-100 focus:text-green-700 border-solid border-2 border-green-2",
+            "rounded-md text-green-2 hover:bg-green-500 hover:text-green-700 hover:text-white/90 focus:bg-green-100 focus:text-green-700 border-solid border-2 border-green-2",
         large: " py-3 px-6 gap-2 ",
         medium: " py-2 px-5 gap-1 rounded-[6px] text-sm ",
         small: " py-2 px-4 gap-1 text-xs rounded-[4px] ",
