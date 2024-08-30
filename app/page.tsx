892b4a93ff0f3d10e6a5f4dc3d6f59ce7fd36889
Alan Vasconcelos <alandbh@gmail.com>
2024-08-29 22:23:43 -0300

    fixing responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,7 @@ export default async function Home() {
         <>
             <Hero data={heroData} />
             <main className="grid grid-cols-12 px-3 md:px-5 max-w-screen-xl mx-auto text-darker-blue">
-                <section className="col-span-10 grid grid-cols-10 col-start-2">
+                <div className="col-span-10 grid grid-cols-10 col-start-2">
                     <Intro
                         title={
                             chamadaDataJson.pages.nodes[0].conteudoPaginaInicial
@@ -112,18 +112,18 @@ export default async function Home() {
                             className="text-sm md:text-[24px] leading-normal flex flex-col gap-4"
                         ></div>
                     </Intro>
-                </section>
+                </div>
             </main>
             <section>
-                <div className="grid grid-cols-12 gap-5 mt-12 bg-[#f2f2f2e6] max-md:flex-col-reverse">
+                <div className="md:grid md:grid-cols-12 max-md:mt-0 max-md:mb-10 gap-5 mt-12 bg-[#f2f2f2e6] max-md:flex max-md:flex-col-reverse">
                     <div
                         dangerouslySetInnerHTML={{
                             __html: destaqueDataJson.pages.nodes[0]
                                 .conteudoPaginaInicial.destaque.texto,
                         }}
-                        className="md:col-start-2 md:col-span-5 2xl:col-start-3 2xl:col-span-3 3xl:col-start-4 3xl:col-span-2 text-xl flex flex-col  gap-y-8 py-10"
+                        className="md:col-start-2 max-md:px-5 md:col-span-5 2xl:col-start-3 2xl:col-span-3 3xl:col-start-4 3xl:col-span-2 text-xl max-md:text-lg flex flex-col gap-y-8 py-10"
                     ></div>
-                    <div className="col-span-6  3xl:col-span-7 w-auto relative">
+                    <div className="md:col-span-6 max-md:h-[300px] 3xl:col-span-7 w-auto relative">
                         <Image
                             className="object-cover"
                             alt=""
@@ -149,7 +149,7 @@ export default async function Home() {
                             </p>
                         </div>
                     </Intro>
-                    <div className="flex flex-col gap-10">
+                    <div className="flex flex-col gap-10 mt-10">
                         {rooms.map((room, index) => (
                             <Card
                                 key={index}
@@ -163,7 +163,7 @@ export default async function Home() {
                     </div>
                 </section>
 
-                <section className="col-span-10 col-start-2 my-20">
+                <section className="col-span-10 col-start-2 max-md:my-10 md:my-20">
                     <Intro title="Algumas fotos" />
 
                     <div className="">
