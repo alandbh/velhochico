9c6f52e407e8b2b507efd4cc04e714cb370279ae
Alan Vasconcelos <alandbh@gmail.com>
2024-09-03 23:23:30 -0300

    Improving NavMenu in  order to identify current page

--- app/_components/NavLink.tsx
+++ app/_components/NavLink.tsx
@@ -10,11 +10,15 @@ type NavLinkProps = {
 
 const NavLink = ({ children, href }: NavLinkProps) => {
     const pathName = usePathname();
+    let path = pathName.split("/");
+    path.shift();
+    const currentPath = href.split("/")[1];
+
     return (
         <li>
             <Link
                 className={`pb-1 px-1 text-white ${
-                    pathName === href
+                    path.includes(currentPath)
                         ? "border-b-4 border-white font-bold hover:border-b-4"
                         : ""
                 } hover:border-b-2 border-white`}
